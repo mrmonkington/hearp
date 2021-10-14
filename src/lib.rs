bd507ceb934be6df9b3c732962c9a244b5d94d96
Mark Kennedy <mark@verynoisy.com>
2021-10-14 11:20:57 +0100

    tested and working for i32 case

--- src/lib.rs
+++ src/lib.rs
@@ -11,25 +11,39 @@ fn _right_id(index: usize) -> usize {
 }
 
 // #[derive(Clone, Copy)]
-struct Hearp<T> {
+pub struct Hearp<T> {
     items: Vec<T>,
     comparison_func: fn(&T, &T) -> bool
 }
 
 impl<T: Clone> Hearp<T> {
     pub fn insert(&mut self, item: T) {
-        self.items.push(item)
+        self.items.push(item);
+        self._upheap(self.items.len() - 1);
     }
 
     pub fn peek(&self) -> Option<&T> {
         return self.items.get(0);
     }
 
+    fn _upheap(&mut self, index: usize) {
+        if index > 0 {
+            let pid: usize = _parent_id(index);
+            if (self.comparison_func)(
+                    &self.items[index],
+                    &self.items[pid]
+                ) {
+                self.items.swap(pid, index);
+                self._upheap(pid);
+            }
+        }
+    }
+
     fn _downheap(&mut self, index: usize) {
         if index < self.items.len() {
             let lid: usize = _left_id(index);
             let rid: usize = _right_id(index);
-            let favourite: usize = index;
+            let mut favourite: usize = index;
 
             if lid < self.items.len()
                 && (self.comparison_func)(
@@ -38,30 +52,46 @@ impl<T: Clone> Hearp<T> {
                 ) {
                 favourite = lid;
             }
+
+            if rid < self.items.len()
+                && (self.comparison_func)(
+                    &self.items[rid],
+                    &self.items[favourite]
+                ) {
+                favourite = rid;
+            }
+
+            // favourite should now contain the parent or one
+            // of the two child items
+            
+            if favourite != index {
+                self.items.swap(index, favourite);
+                self._downheap(favourite);
+            }
         }
     }
 
-    fn pop(&mut self) -> Option<T> {
+    pub fn pop(&mut self) -> Option<T> {
         // gotta clone it out since we'll be removing it
         let v = self.items.first().cloned();
         match v {
             Some(_) => {
                 let last = self.items.pop();
                 match last {
-                    Some(v_last) => {
-                        self.items.insert(0, v_last);
+                    Some(v_last) if self.items.len() > 0 => {
+                        self.items[0] = v_last;
                         self._downheap(0);
                     },
-                    None => {
+                    _ => {
                         // do nothing, heap is now empty
                     }
                 }
             },
-            None => {
+            _ => {
                 // heap is empty
             }
         }
-        return v
+        return v;
     }
 }
 
@@ -98,14 +128,22 @@ mod tests {
     fn test_ints() {
         // ranges are an iterator type and need collecting before using as a 
         // contained sequence
-        let mut src: Vec<i32> = (0..100).collect();
-        src.shuffle(&mut thread_rng());
+        let src: Vec<i32> = (0..100).collect();
+        let mut randsrc = src.clone();
+        randsrc.shuffle(&mut thread_rng());
         let mut heep: Hearp<i32> = Hearp {
             items: Vec::new(),
             comparison_func: |x:&i32, y:&i32| x < y
         };
-        for item in src.iter() {
-            heep.insert(*item);
+        for item in randsrc.iter() {
+            // don't want to borrow it out of the src list since we want to do a comparison
+            // later
+            heep.insert(item.clone());
         }
+        let mut op: Vec<i32> = Vec::new();
+        while let Some(item) = heep.pop() {
+            op.push(item);
+        }
+        assert_eq!(op, src);
     }
 }
