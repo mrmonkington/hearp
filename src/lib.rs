b8bfe42b43edddc258a31fa1655c1394d6a1189f
Mark Kennedy <mark@verynoisy.com>
2021-10-15 14:29:13 +0100

    refactor to support all callable types, not just func pts; added ::new

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,19 @@ fn _right_id(index: usize) -> usize {
 }
 
 // #[derive(Clone, Copy)]
-pub struct Hearp<T> {
+pub struct Hearp<T: Clone, F: Fn(&T, &T) -> bool> {
     items: Vec<T>,
-    comparison_func: fn(&T, &T) -> bool
+    comparison_func: F
 }
 
-impl<T: Clone> Hearp<T> {
+impl<T: Clone, F: Fn(&T, &T) -> bool> Hearp<T, F> {
+    pub fn new(comparison_func: F) -> Hearp<T, F> {
+        Hearp {
+            items: Vec::new(),
+            comparison_func
+        }
+    }
+
     pub fn insert(&mut self, item: T) {
         self.items.push(item);
         self._upheap(self.items.len() - 1);
@@ -131,16 +138,41 @@ mod tests {
         let src: Vec<i32> = (0..100).collect();
         let mut randsrc = src.clone();
         randsrc.shuffle(&mut thread_rng());
-        let mut heep: Hearp<i32> = Hearp {
-            items: Vec::new(),
-            comparison_func: |x:&i32, y:&i32| x < y
-        };
+        let mut heep = Hearp::new( |x:&i32, y:&i32| x < y );
         for item in randsrc.iter() {
             // don't want to borrow it out of the src list since we want to do a comparison
             // later
             heep.insert(item.clone());
         }
         let mut op: Vec<i32> = Vec::new();
+        while let Some(item) = heep.pop() {
+            op.push(item);
+        }
+        assert_eq!(op, src);
+    }
+
+    #[test]
+    fn test_things() {
+        // ranges are an iterator type and need collecting before using as a 
+        // contained sequence
+      
+        // PartialEq asserts that this struct is equal
+        #[derive(Debug, Clone, PartialEq)]
+        struct Thing {
+            value: i32
+        }
+        
+        let src: Vec<Thing> = (0..100).map(|x| Thing{value: x}).collect();
+        dbg!(&src);
+        let mut randsrc = src.clone();
+        randsrc.shuffle(&mut thread_rng());
+        let mut heep = Hearp::new( |x:&Thing, y:&Thing| x.value < y.value );
+        for item in randsrc.iter() {
+            // don't want to borrow it out of the src list since we want to do a comparison
+            // later
+            heep.insert(item.clone());
+        }
+        let mut op: Vec<Thing> = Vec::new();
         while let Some(item) = heep.pop() {
             op.push(item);
         }
